03ba8384d3883ae9af7376c943a00f7290d3e9dc
Nuno Valente <nuno.valente91@gmail.com>
2020-12-22 14:53:29 +0000

    Finished Project

--- app/src/main/java/com/udacity/shoestore/databinding/GlideBindingAdapter.kt
+++ app/src/main/java/com/udacity/shoestore/databinding/GlideBindingAdapter.kt
@@ -1,7 +1,5 @@
 package com.udacity.shoestore.databinding
 
-import android.content.Context
-import android.net.Uri
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
@@ -9,25 +7,14 @@ import com.udacity.shoestore.R
 
 class GlideBindingAdapter {
 
-  /*  @BindingAdapter("imageUrl")
-    public static void setImageResource(ImageView view, String imageUrl) {
-        Context context = view.getContext();
+    companion object {
+        @JvmStatic
+        @BindingAdapter("imageUrl")
+        fun setImageResource(imageView: ImageView, imageUrl: String) {
+            val context = imageView.context
 
-        /*    RequestOptions options = new RequestOptions()
-                     .dontAnimate()
-                     .placeholder(R.drawable.default_account)
-                     .error(R.drawable.default_account);*/
-
-        GlideApp.with(context)
-            .load(imageUrl)
-            .placeholder(R.drawable.default_account)
-            .into(view);
-    }*/
-
-    @BindingAdapter("imageUrl")
-    fun setImageResource(imageView: ImageView, imageUrl: String) {
-        val context = imageView.context
-
-        Glide.with(context).load(Uri.parse(imageUrl)).placeholder(R.drawable.default_image).into(imageView)
+            Glide.with(context).load(imageUrl).placeholder(R.drawable.default_image)
+                .into(imageView)
+        }
     }
 }
